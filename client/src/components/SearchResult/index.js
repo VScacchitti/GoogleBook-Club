b635c2d80795992cf74a772c36609d128342ffff
Vince Scacchitti <VScacchitti24@gmail.com>
2020-12-30 07:37:35 -0500

    addrd Search result component which uses props to populate cards with appropriate data from the search. The logic will be handled ont he search page js.

---
+++ client/src/components/SearchResult/index.js
@@ -0,0 +1,37 @@
+
+import React from "react";
+import { Container, Row, Col } from "../Grid"
+
+const SearchResult = (props) => {
+    return (
+        <Container>
+            <h3>Book Results</h3>
+            {props.books.map(book => {
+                return (
+                    <div className="card mb-5">
+                        <div className="card-body">
+                            <Row className="row" id={book.title + "Card"} key={book._id}>
+                                <Col size="2">
+                                    <img src={book.image} alt={book.title}/>
+                                </Col>
+                                <Col size="10" className="pl-2">
+                                <h3>{book.title}</h3>
+                                <h4>{book.author}</h4>
+                                <p className="pr-3">{book.desciption}</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <button className="save btn mt-4 ml-3 mr-1">Save Book</button>
+                                <a href={book.link} target="_blank" rel="noopener noreferrer">
+                                <button className="view btn mt-4">View Book</button>
+                                </a>
+                            </Row>
+
+                        </div>
+                    </div>
+                )
+            })}
+
+        </Container>
+    )
+}
