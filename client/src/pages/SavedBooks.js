a0966cfda58b1ae00bf8bf1c8a11d21707f3e874
Vince Scacchitti <VScacchitti24@gmail.com>
2020-12-30 12:55:12 -0500

    not saving correctly yet, but finished the coding for the saved book page.

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -5,5 +5,29 @@ import SavedResult from "../components/SavedResult"
 
 class SavedBook extends Component {
 
-    
+    state = {
+        savedBooks = []
+    };
+  //calls to the API to get saved books
+    componentDidMount() {
+        API.getBooks()
+           .then( res => this.setState({savedBooks: res.data}))
+           .catch(err=> console.log(err))
+    }
+
+    //handle Delete Book
+    handleDeleteButton = id => {
+        API.deleteBook(id)
+           .then( res => this.componentDidMount())
+           .catch(err => console.log(err))
+    }
+
+    render() {
+        return (
+            <SavedResult savedBooks={this.state.savedBooks} handleDeleteButton={this.handleDeleteButton} />
+        )
+    }
+
 }
+
+export default SavedBook
