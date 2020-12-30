3c29f80a0c5e39b7dafe381868e500f117491aa3
Vince Scacchitti <VScacchitti24@gmail.com>
2020-12-30 14:14:01 -0500

    search working again, returning to the page.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -3,13 +3,13 @@ import API from "../utils/API";
 import { Container, Row, Col } from "../components/Grid";
 import Search from "../components/Search";
 import SearchResult from "../components/SearchResult";
-import SearchBook from "../components/Search";
+
 
 
 class SearchBooks extends Component {
 
     state = {
-        search: "",
+        search:"",
         books:[],
         error:"",
     };
@@ -17,41 +17,46 @@ class SearchBooks extends Component {
     //handle input chnage on our search
     handleInputChange = event => {
         this.setState({search: event.target.value})
+        console.log(event.target.value)
     }
 
     //Handles the submissio of the form
     handleFormSubmit = event => {
         event.preventDefault();
-        API.getGoogleBooks(this.state.search)
-           .then(res => {
-               if (res.data.items === "error") {
-                   throw new Error(res.data.items)
-               }
-               else {
-                   let results = res.data.items
-
-                   results = results.map( result => {
-                       result = {
-                           key: result.id,
-                           id: result.id,
-                           title: result.volumeInfo.title,
-                           authors: result.volumeInfo.authors,
-                           description: results.volumeInfo.description,
-                           image: results.volumeInfo.imageLinks.thumbnail,
-                           link: result.volumeInfo.infoLink
-                       }
-                       return result
-                   })
-                   this.setState({books: results, search:""})
-               }
-           })
-           .catch(err=>this.setState({error:err.items}))
-    }
+        API.getGoogleSearchBooks(this.state.search)
+          .then(res => {
+            if (res.data.items === "error") {
+              throw new Error(res.data.items);
+            }
+            else {
+              // store response
+              let results = res.data.items
+              //map through the array 
+              results = results.map(result => {
+                //store each book information in a new object 
+                result = {
+                  key: result.id,
+                  id: result.id,
+                  title: result.volumeInfo.title,
+                  authors: result.volumeInfo.authors,
+                  description: result.volumeInfo.description,
+                  image: result.volumeInfo.imageLinks.thumbnail,
+                  link: result.volumeInfo.infoLink
+                }
+                return result;
+              })
+              this.setState({ books: results, search: ""})
+            }
+          })
+          .catch(err => this.setState({ error: err.items }));
+      }
 
     //handles saving a book
 
     handleSavedButton = event => {
+        
         event.preventDefault();
+        
         let savedBooks = this.state.books.filter(book => book.id===event.target.id)
         API.saveBook(savedBooks)
            .then(console.log(savedBooks))
