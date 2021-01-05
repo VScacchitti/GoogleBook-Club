7e24e0a7b4b46af9f8f2496641975173209fd532
Vince Scacchitti <VScacchitti24@gmail.com>
2021-01-05 13:53:53 -0500

    reworking components.

--- client/src/components/SavedResult/index.js
+++ client/src/components/SavedResult/index.js
@@ -1,60 +1,42 @@
 import React from "react";
-import "./style.css"
-import {Row, Col} from "../Grid"
+import { Container, Row, Col } from "../Grid";
+import "./style.css";
 
 const SavedResult = props => {
-    return (props.savedBooks.length === 0) ? (
-        <div className="card">
-            <div className="card-body player">
-                <div className="article">
-                    <h3>Books that You Saved</h3>
-                </div>
+  return (
+    <Container>
+      <h3>Saved Books</h3>
+      {props.savedBooks.map(savedbook => {
+        return (
+          <div>
+            <div className="card mb-2">
+              <div className="card-body">
+                <Row id={savedbook.title + "Card"} key={savedbook._id}>
+                  <Col size="2">
+                    <img src={savedbook.image} alt={savedbook.title} />
+                  </Col>
+                  <Col size="10" className="pl-2">
+                    <h3 className="bookTitle">{savedbook.title}</h3>
+                    <h4 className="bookAuthor">{savedbook.authors}</h4>
+                    <p className="bookDescription pr-3">{savedbook.description}</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <button className="delete btn mt-4 ml-3 mr-1" id={savedbook._id} onClick={() => props.handleDeleteButton(savedbook._id)}>
+                    Delete Book</button>
+                  <a href={savedbook.link} target="_blank" rel="noopener noreferrer">
+                    <button className="view btn mt-4">
+                      View Book</button>
+                  </a>
+                </Row>
+              </div>
             </div>
-        </div>
-    ):(
-        <div className="card">
-            <div className="card-body player">
-                <div className="article">
-                    <h3>Books that You Saved</h3>
-                    {props.savedBooks.map(savedbook => {
-                        return (
-                            <li className="saved-list list-group-item">
-                                <Row className="SearchResult" id={savedbook.title + "Card"} key={savedbook._id}>
-                                    {/* col-3 show image of the book */}
-                                    <Col size="2" className="bookImage">
-                                        <img src={savedbook.image} alt={savedbook.title} />
-                                    </Col>
-                                    <Col size="1" className="emptyCol"/>
-                                    {/* col-9 show information of the book */}
-                                    <Col size="9" className="bookInfo">
-                                        <Row>
-                                            <h2 className="bookTitle">{savedbook.title}</h2>
-                                        </Row>
-                                        <Row>
-                                            <h3 className="bookAuthor">{savedbook.authors}</h3>
-                                        </Row>
-                                        <Row>
-                                            <p className="bookDescription">{savedbook.description}</p>
-                                        </Row>
-                                    </Col>
-                                </Row>
-                                <br></br>
-                                <Row className="buttonDiv ">
-                                    <button className="deleteBook btn btn-danger" id={savedbook._id} onClick={() => props.handleDeleteButton(savedbook._id)}>
-                                        Delete Book
-                                    </button>
-                                    <a href={savedbook.link} target="_blank">
-                                        <button className="viewBook btn btn-success">
-                                            View Book
-                                        </button>
-                                    </a>
-                                </Row>
-                            </li>
-                        );
-                    })}
-                </div>
-            </div>
-        </div>
-    )
+          </div>
+        );
+      })
+      }
+    </Container>
+  )
 }
-export default SavedResult
+
+export default SavedResult;
