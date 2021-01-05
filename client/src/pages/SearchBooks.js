419cba48058ebbcd4911037cc8ca88cb9e980d52
Vince Scacchitti <VScacchitti24@gmail.com>
2021-01-05 14:35:17 -0500

    Search Working again and displaying on page, had to look through old commits to refind my working code. insert eye roll

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,96 +1,78 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import Jumbotron from "../components/Jumbotron";
 import { Container, Row, Col } from "../components/Grid";
 import SearchForm from "../components/SearchForm";
 import SearchResult from "../components/SearchResult"
 
 
-
 class SearchBooks extends Component {
+  state = {
+    search: "",
+    books: [],
+    error: "",
+  };
 
-    state = {
-        search: "",
-        books: [],
-        error: "",
-        message: ""
-    };
+  handleInputChange = event => {
+    this.setState({ search: event.target.value })
+  }
 
-    //handle input chnage on our search
-    handleInputChange = event => {
-        this.setState({ search: event.target.value })
-    }
+  //function to control the submit button of the search form 
+  handleFormSubmit = event => {
+    event.preventDefault();
+    API.getGoogleSearchBooks(this.state.search)
+      .then(res => {
+        if (res.data.items === "error") {
+          throw new Error(res.data.items);
+        }
+        else {
+          // store response
+          let results = res.data.items
+          //map through the array 
+          results = results.map(result => {
+            //store each book information in a new object 
+            result = {
+              key: result.id,
+              id: result.id,
+              title: result.volumeInfo.title,
+              authors: result.volumeInfo.authors,
+              description: result.volumeInfo.description,
+              image: result.volumeInfo.imageLinks.thumbnail,
+              link: result.volumeInfo.infoLink
+            }
+            return result;
+          })
+          this.setState({ books: results, search: ""})
+        }
+      })
+      .catch(err => this.setState({ error: err.items }));
+  }
 
-    //Handles the submissio of the form
-    handleFormSubmit = event => {
-        event.preventDefault();
-        // once it clicks it connects to the google book api with the search value
-        API.getGoogleSearchBooks(this.state.search)
-            .then(res => {
-                if (res.data.items === "error") {
-                    throw new Error(res.data.items);
-                }
-                else {
-                    // store response in a array
-                    let results = res.data.items
-                    //map through the array 
-                    results = results.map(result => {
-                        //store each book information in a new object 
-                        result = {
-                            key: result.id,
-                            id: result.id,
-                            title: result.volumeInfo.title,
-                            author: result.volumeInfo.authors,
-                            description: result.volumeInfo.description,
-                            image: result.volumeInfo.imageLinks.thumbnail,
-                            link: result.volumeInfo.infoLink
-                        }
-                        return result;
-                    })
-                    // reset the sate of the empty books array to the new arrays of objects with properties geting back from the response
-                    this.setState({ books: results, error: "" })
-                }
-            })
-            .catch(err => this.setState({ error: err.items }));
-    }
 
-    //handles saving a book
-
-    handleSavedButton = event => {
-        // console.log(event)
-        event.preventDefault();
-        console.log(this.state.books)
-        let savedBooks = this.state.books.filter(book => book.id === event.target.id)
-        savedBooks = savedBooks[0];
-        API.saveBook(savedBooks)
-            .then(this.setState({ message: alert("Your book is saved") }))
-            .catch(err => console.log(err))
-    }
-
-    render() {
-      return (
-        <Container fluid>
-        <Jumbotron>
-            <h1 className="text-white">Find Your Favorite Books with GoogleBook API</h1>
-        </Jumbotron>
+  handleSavedButton = event => {
+    event.preventDefault();
+    let savedBooks = this.state.books.filter(book => book.id === event.target.id)
+    API.saveBook(savedBooks)
+      .then(console.log(savedBooks))
+      .catch(err => console.log(err));
+  };
+  render() {
+    return (
+      <Container fluid>
         <Container>
-            <Row>
-                <Col size="12">
-                    <SearchForm
-                        handleFormSubmit={this.handleFormSubmit}
-                        handleInputChange={this.handleInputChange}
-                    />
-                </Col>
-            </Row>
+          <Row>
+            <Col size="12">
+              <SearchForm
+                handleFormSubmit={this.handleFormSubmit}
+                handleInputChange={this.handleInputChange}
+              />
+            </Col>
+          </Row>
         </Container>
-        <br></br>
         <Container>
-            <SearchResult books={this.state.books} handleSavedButton={this.handleSavedButton} />
+          <SearchResult books={this.state.books} handleSavedButton={this.handleSavedButton} />
         </Container>
-    </Container>
+      </Container>
     )
-    }
-
+  }
 }
-
 export default SearchBooks;
