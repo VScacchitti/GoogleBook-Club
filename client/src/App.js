83b29002697c31a95b4929fbf0c32cf72792ebb4
Vince Scacchitti <VScacchitti24@gmail.com>
2021-01-05 15:13:29 -0500

    save books now working.

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,27 @@
-import React, {Component} from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import SearchBooks from "./pages/SearchBooks"
-import SavedBooks from "./pages/SavedBooks"
-import Nav from "./components/Nav"
+import SaveBooks from "./pages/SaveBooks";
+import SearchBooks from "./pages/SearchBooks";
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron"
 import Footer from "./components/Footer"
-import "./App.css";
+import "./App.css"
 
 
-class App extends Component {
-  render(){
-    return (
-      <Router>
-        <div>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={SearchBooks}/>
-            <Route exact path="/saved" component={SavedBooks}/>
-            <Route exact path="/saved/:id" component={SavedBooks} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    )
-  }
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Jumbotron/>
+        <Switch>
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/saved" component={SaveBooks} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
-
 
 export default App;
